16eae9dc56238f2d76156ea6053ebf99de7b5693
Szymon Cierpik <109699547+SzymonCierpik@users.noreply.github.com>
2023-02-16 16:39:43 +0100

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -1,8 +1,20 @@
 const numerOfCategory = document.querySelectorAll("li.item");
-console.log("Numer of categories:", numerOfCategory.length);
+console.log("Numer of categories: ", numerOfCategory.length);
 
 const firstNameCategory = document.querySelector("h2");
-console.log("Category:", firstNameCategory.textContent);
+console.log("Category: ", firstNameCategory.textContent);
 
-const elementsName = document.querySelectorAll("li.item");
-console.log("Elements:", elementsName[0].children[1].children.length);
+const oneElementsName = document.querySelectorAll("li.item");
+console.log("Elements: ", oneElementsName[0].children[1].children.length);
+
+const secondNameCategory = document.querySelectorAll("h2");
+console.log("Category: ", secondNameCategory[1].textContent);
+
+const twoElementsName = document.querySelectorAll("li.item");
+console.log("Elements: ", oneElementsName[1].children[1].children.length);
+
+const thirdNameCategory = document.querySelectorAll("h2");
+console.log("Category: ", thirdNameCategory[2].textContent);
+
+const threeElementsName = document.querySelectorAll("li.item");
+console.log("Elements: ", threeElementsName[2].children[1].children.length);
