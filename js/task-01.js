f8587bd85990b969c62866e880593806e4ea5270
Szymon Cierpik <109699547+SzymonCierpik@users.noreply.github.com>
2023-02-16 11:24:18 +0100

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -1,2 +1,8 @@
-const numerOfCategory = document.querySelectorAll('li.item');
-console.log("Numer of categories", numerOfCategory.length);
+const numerOfCategory = document.querySelectorAll("li.item");
+console.log("Numer of categories:", numerOfCategory.length);
+
+const firstNameCategory = document.querySelector("h2");
+console.log("Category:", firstNameCategory.textContent);
+
+const elementsName = document.querySelectorAll("li.item");
+console.log("Elements:", elementsName[0].children[1].children.length);
