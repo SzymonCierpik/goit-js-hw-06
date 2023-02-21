599d4d1f89667cc75b55bb62965fcd3f8eb3d009
Szymon Cierpik <109699547+SzymonCierpik@users.noreply.github.com>
2023-02-21 16:43:19 +0100

    1

---
+++ js/task-06.js
@@ -0,0 +1,12 @@
+const textInput = document.querySelector("#validation-input");
+const textInputLength = Number(textInput.getAttribute("data-length"));
+
+textInput.addEventListener("blur", (event) => {
+  if (event.currentTarget.value.length === textInputLength) {
+    textInput.classList.add("valid");
+    textInput.classList.remove("invalid");
+  } else {
+    textInput.classList.add("invalid");
+    textInput.classList.remove("valid");
+  }
+});
