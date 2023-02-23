ee994bca046adc3c89edc621ffbea9c471198b55
Szymon Cierpik <109699547+SzymonCierpik@users.noreply.github.com>
2023-02-23 09:23:29 +0100

    1

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,21 @@
+const loginForm = document.querySelector(".login-form");
 
+loginForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const email = loginForm.elements.email.value;
+  const password = loginForm.elements.password.value;
+
+  if (email === "" || password === "") {
+    alert("Proszę wypełnić wszystkie pola!");
+    return;
+  }
+
+  const formData = {
+    email: email,
+    password: password,
+  };
+
+  console.log(formData);
+  loginForm.reset();
+});
